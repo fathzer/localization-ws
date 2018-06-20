fc84adcdb2ccd5d290108826b9924db4b8d56eb6
jma <jma@jma-M51AC>
2018-06-20 20:14:00 +0200

    Makes the RequestMapping declaration more compact

--- src/main/java/com/fathzer/localization/ws/controller/MessagesController.java
+++ src/main/java/com/fathzer/localization/ws/controller/MessagesController.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ import com.fathzer.localization.URLMessageBuilder;
 
 @RestController
 public class MessagesController {
-	@RequestMapping(value="v1/messages", produces = {"application/JSON"}, method = RequestMethod.GET)
+	@GetMapping(value="v1/messages", produces = {"application/JSON"})
 	public Map<String,String> get(@RequestParam(value="url") String url, @RequestParam(value="country") String country, @RequestParam(value="lng") String lng, @RequestParam(value="id", required=false) List<String> id) {
 		if (id==null) {
 			return URLMessageBuilder.getMessages(URLMessageBuilder.parseURL(url), new Locale(lng, country));
